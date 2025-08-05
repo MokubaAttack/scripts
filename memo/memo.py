9edefda03ed1bf2d6b0cc842f426711292b376d8
mokuba_attack <ninjapink24@gmail.com>
2025-08-05 23:36:10 +0900

    Update memo.py

--- memo/memo.py
+++ memo/memo.py
@@ -7,7 +7,7 @@ pipe=StableDiffusionXLPipeline.from_single_file(ckpt_path,torch_dtype=torch.floa
 prompt=
 #list of tokenids
 prompt_ids = pipe.tokenizer(prompt).input_ids
-
+,return_tensors="pt"
 #get tokenid from token
 tokenid = pipe.tokenizer.convert_tokens_to_ids(token)
 
