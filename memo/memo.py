eccc74d34e2c3f4a082d262e43e177242eefeb8e
mokuba_attack <ninjapink24@gmail.com>
2025-08-06 09:32:51 +0900

    Update memo.py

--- memo/memo.py
+++ memo/memo.py
@@ -1,17 +1,20 @@
+# I want to make Embedding.
 from diffusers import StableDiffusionXLPipeline
 import torch
 
 ckpt_path=
 pipe=StableDiffusionXLPipeline.from_single_file(ckpt_path,torch_dtype=torch.float16)
 
-prompt=
-#list of tokenids
-prompt_ids = pipe.tokenizer(prompt).input_ids
-,return_tensors="pt"
-#get tokenid from token
-tokenid = pipe.tokenizer.convert_tokens_to_ids(token)
-
-#reverse
-for tokenid in tokenids:
-  token = pipe.tokenizer.convert_ids_to_tokens(tokenid)
-pipe.text_encoder
+ex_prompt="8k, RAW photo, best quality, masterpiece"
+ex_prompt_list=ex_prompt.split(",")
+ex_prompt_embs=[]
+ex_prompt_embs2=[]
+for ex_prompt_elem in ex_prompt_list:
+  ex_prompt_elem_ids = pipe.tokenizer(ex_prompt_elem,return_tensors="pt,truncation=False).input_ids
+  ex_prompt_elem_tensor = pipe.text_encoder(ex_prompt_elem_ids)[0]
+  ex_prompt_elem_ids2 = pipe.tokenizer_2(ex_prompt_elem,return_tensors="pt,truncation=False).input_ids
+  ex_prompt_elem_tensor2 = pipe.text_encoder_2(ex_prompt_elem_ids2)[0]
+  ex_prompt_embs.append(ex_prompt_elem_tensor)
+  ex_prompt_embs2.append(ex_prompt_elem_tensor2)
+clip_l_tensors=torch.cat(ex_prompt_embs, dim=0)
+clip_g_tensors=torch.cat(ex_prompt_embs2, dim=0)
