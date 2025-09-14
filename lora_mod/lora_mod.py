6fc7d5f161104c018c0b497cc95bcfd50edcb8e0
mokuba_attack <ninjapink24@gmail.com>
2025-09-14 19:06:08 +0900

    Update lora_mod.py

--- lora_mod/lora_mod.py
+++ lora_mod/lora_mod.py
@@ -1,13 +1,6 @@
-import os
 from safetensors.torch import load_file,save_file
-import FreeSimpleGUI as sg
-
-layout =[
-    [sg.InputText(key='-INPUT-'),sg.FileBrowse('select lora', key="-FILENAME-", file_types=(('lora file', '.safetensors'),))],
-    [sg.Button('Exit'),sg.Button('Run',key="-RUN-")]
-        ]
-
-window = sg.Window('Modify Lora', layout)
+import sys
+import os
 
 ok=[
     "lora_unet_out_2.alpha",
@@ -30,32 +23,20 @@ ok=[
     "lora_unet_time_embed_2.lora_up.weight"
 ]
 
-while True:
-    event, values = window.read()
-    if event in (sg.WIN_CLOSED, 'Exit'):
-        break
-    elif event=="-RUN-":
-        if values['-INPUT-']!="":
-            if os.path.exists(values['-INPUT-']):
-                if values['-INPUT-'].endswith(".safetensors"):
-                    try:
-                        in_path=values['-INPUT-']
-                        out=in_path.replace(".safetensors","_mod.safetensors")
-                        state_dict=load_file(in_path)
-                        out_dict={}
-                        for k,w in state_dict.items():
-                            if not(k in ok):
-                                out_dict[k]=w
-                        save_file(out_dict,out)
-                        sg.popup(out,title="fin")
-                    except:
-                        sg.popup("fail in the output.",title="error")
-                else:
-                    sg.popup("you need to select the safetensors file.",title="error")
-            else:
-                sg.popup("the selected file doesn't exist.",title="error")
-        else:
-            sg.popup("you need to input the file path.",title="error")
+try:
+    path = sys.argv[1]
+except:
+    path = 1
+if path!=1:
+    if path.endswith(".safetensors"):
+        try:
+            state_dict=load_file(path)
+            for k in ok:
+                if k in state_dict:
+                    out=state_dict.pop(k)
+            out_path=path.replace(".safetensors","_mod.safetensors")
+            save_file(state_dict,out_path)
+            del state_dict
+        except:
+            print(path+" is failed.")
             
-window.close()
-
