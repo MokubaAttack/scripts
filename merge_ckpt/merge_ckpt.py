e17691cb7c84d5511e44777f578e347eed9336b3
mokuba_attack <ninjapink24@gmail.com>
2025-10-07 16:49:45 +0900

    Update merge_ckpt.py

--- merge_ckpt/merge_ckpt.py
+++ merge_ckpt/merge_ckpt.py
@@ -9,15 +9,17 @@ import threading
 
 sg.theme('GrayGrayGray')
 
-def mergeckpt(ckpts,weights,v,out_path):
-    w.find_element('RUN').Update(disabled=True)
+def mergeckpt(ckpts,weights,v,out_path,win):
+    win.find_element('RUN').Update(disabled=True)
     if not(out_path.endswith(".safetensors")):
-        w.find_element('RUN').Update(disabled=False)
+        win.find_element('RUN').Update(disabled=False)
+        win["info"].update("error")
         notification.notify(title="error",message=path+" does not exist.",timeout=8)
         return
     for path in ckpts:
         if not(os.path.exists(path)):
-            w.find_element('RUN').Update(disabled=False)
+            win.find_element('RUN').Update(disabled=False)
+            win["info"].update("error")
             notification.notify(title="error",message="I failed in the output.",timeout=8)
             return
     try:
@@ -26,6 +28,7 @@ def mergeckpt(ckpts,weights,v,out_path):
             weights[i]=weights[i]/weights_sum
         out_dict={}
         safe=open(os.getcwd()+"/data.txt","r")
+        win["info"].update("making initial ckpt")
         for line in safe:
             data=line.split(",")
             s=[]
@@ -38,11 +41,16 @@ def mergeckpt(ckpts,weights,v,out_path):
         safe.close()
         for i in range(len(ckpts)):
             state_dict=load_file(ckpts[i])
+            dict_sum=len(out_dict.keys())
+            key_count=0
             for k,w in out_dict.items():
+                key_count=key_count+1
+                win["info"].update("merging "+os.path.basename(ckpts[i])+" "+str(key_count)+"/"+str(dict_sum))
                 if k in state_dict:
                     out_dict[k]=out_dict[k].to(torch.float16)+(state_dict[k]*weights[i]).to(torch.float16)
         if v!=-1:
             state_dict=load_file(ckpts[v])
+            win["info"].update("baking vae")
             for k,w in out_dict.items():
                 if k.startswith("first_stage_model."):
                     out_dict[k]=state_dict[k].to(torch.float16)
@@ -56,16 +64,19 @@ def mergeckpt(ckpts,weights,v,out_path):
             f.write("vae : None\n")
         f.close()
         del out_dict,state_dict
-        w.find_element('RUN').Update(disabled=False)
+        win.find_element('RUN').Update(disabled=False)
+        win["info"].update("fin")
         notification.notify(title="fin",message=out_path,timeout=8)
     except:
-        w.find_element('RUN').Update(disabled=False)
+        win.find_element('RUN').Update(disabled=False)
+        win["info"].update("error")
         notification.notify(title="error",message="I failed in the output.",timeout=8)
         
 keys=[
     "ckpt1","ckpt2","ckpt3","ckpt4","ckpt5","ckpt6","ckpt7",
     "w1","w2","w3","w4","w5","w6","w7","out"
-]        
+]
+grp_rclick_menu={}
 for key in keys:
     grp_rclick_menu[key]=[
         "",
@@ -74,42 +85,23 @@ for key in keys:
         ]
     ]
 
-box1=[
-    [sg.Text("checkpoint file")],
-    [sg.Column([[sg.Text("ckpt1"), sg.Input(key="ckpt1",right_click_menu=grp_rclick_menu["ckpt1"]),sg.FileBrowse( file_types=(('ckpt file', '.safetensors'),))]],size=(435,35))],
-    [sg.Column([[sg.Text("ckpt2"), sg.Input(key="ckpt2",right_click_menu=grp_rclick_menu["ckpt2"]),sg.FileBrowse( file_types=(('ckpt file', '.safetensors'),))]],size=(435,35))],
-    [sg.Column([[sg.Text("ckpt3"), sg.Input(key="ckpt3",right_click_menu=grp_rclick_menu["ckpt3"]),sg.FileBrowse( file_types=(('ckpt file', '.safetensors'),))]],size=(435,35))],
-    [sg.Column([[sg.Text("ckpt4"), sg.Input(key="ckpt4",right_click_menu=grp_rclick_menu["ckpt4"]),sg.FileBrowse( file_types=(('ckpt file', '.safetensors'),))]],size=(435,35))],
-    [sg.Column([[sg.Text("ckpt5"), sg.Input(key="ckpt5",right_click_menu=grp_rclick_menu["ckpt5"]),sg.FileBrowse( file_types=(('ckpt file', '.safetensors'),))]],size=(435,35))],
-    [sg.Column([[sg.Text("ckpt6"), sg.Input(key="ckpt6",right_click_menu=grp_rclick_menu["ckpt6"]),sg.FileBrowse( file_types=(('ckpt file', '.safetensors'),))]],size=(435,35))],
-    [sg.Column([[sg.Text("ckpt7"), sg.Input(key="ckpt7",right_click_menu=grp_rclick_menu["ckpt7"]),sg.FileBrowse( file_types=(('ckpt file', '.safetensors'),))]],size=(435,35))]
-]
-
-box2=[
-    [sg.Text("weight")],
-    [sg.Column([[sg.Input(key="w1",right_click_menu=grp_rclick_menu["w1"])]],size=(80,35))],
-    [sg.Column([[sg.Input(key="w2",right_click_menu=grp_rclick_menu["w2"])]],size=(80,35))],
-    [sg.Column([[sg.Input(key="w3",right_click_menu=grp_rclick_menu["w3"])]],size=(80,35))],
-    [sg.Column([[sg.Input(key="w4",right_click_menu=grp_rclick_menu["w4"])]],size=(80,35))],
-    [sg.Column([[sg.Input(key="w5",right_click_menu=grp_rclick_menu["w5"])]],size=(80,35))],
-    [sg.Column([[sg.Input(key="w6",right_click_menu=grp_rclick_menu["w6"])]],size=(80,35))],
-    [sg.Column([[sg.Input(key="w7",right_click_menu=grp_rclick_menu["w7"])]],size=(80,35))]
-]
-
-box3=[
-    [sg.Text("vae")],
-    [sg.Column([[sg.Radio("",key="v1",group_id='destination')]],size=(35,35))],
-    [sg.Column([[sg.Radio("",key="v2",group_id='destination')]],size=(35,35))],
-    [sg.Column([[sg.Radio("",key="v3",group_id='destination')]],size=(35,35))],
-    [sg.Column([[sg.Radio("",key="v4",group_id='destination')]],size=(35,35))],
-    [sg.Column([[sg.Radio("",key="v5",group_id='destination')]],size=(35,35))],
-    [sg.Column([[sg.Radio("",key="v6",group_id='destination')]],size=(35,35))],
-    [sg.Column([[sg.Radio("",key="v7",group_id='destination')]],size=(35,35))]
-]
-
 layout=[
-    [sg.Column(box1),sg.Column(box2),sg.Column(box3)],
+    [sg.Text("ckpt1"), sg.Input(key="ckpt1",right_click_menu=grp_rclick_menu["ckpt1"]),sg.FileBrowse( file_types=(('ckpt file', '.safetensors'),))],
+    [sg.Text("weight"),sg.Input(key="w1",right_click_menu=grp_rclick_menu["w1"]),sg.Text("vae"),sg.Radio("",key="v1",group_id='destination')],
+    [sg.Text("ckpt2"), sg.Input(key="ckpt2",right_click_menu=grp_rclick_menu["ckpt2"]),sg.FileBrowse( file_types=(('ckpt file', '.safetensors'),))],
+    [sg.Text("weight"),sg.Input(key="w2",right_click_menu=grp_rclick_menu["w2"]),sg.Text("vae"),sg.Radio("",key="v2",group_id='destination')],
+    [sg.Text("ckpt3"), sg.Input(key="ckpt3",right_click_menu=grp_rclick_menu["ckpt3"]),sg.FileBrowse( file_types=(('ckpt file', '.safetensors'),))],
+    [sg.Text("weight"),sg.Input(key="w3",right_click_menu=grp_rclick_menu["w3"]),sg.Text("vae"),sg.Radio("",key="v3",group_id='destination')],
+    [sg.Text("ckpt4"), sg.Input(key="ckpt4",right_click_menu=grp_rclick_menu["ckpt4"]),sg.FileBrowse( file_types=(('ckpt file', '.safetensors'),))],
+    [sg.Text("weight"),sg.Input(key="w4",right_click_menu=grp_rclick_menu["w4"]),sg.Text("vae"),sg.Radio("",key="v4",group_id='destination')],
+    [sg.Text("ckpt5"), sg.Input(key="ckpt5",right_click_menu=grp_rclick_menu["ckpt5"]),sg.FileBrowse( file_types=(('ckpt file', '.safetensors'),))],
+    [sg.Text("weight"),sg.Input(key="w5",right_click_menu=grp_rclick_menu["w5"]),sg.Text("vae"),sg.Radio("",key="v5",group_id='destination')],
+    [sg.Text("ckpt6"), sg.Input(key="ckpt6",right_click_menu=grp_rclick_menu["ckpt6"]),sg.FileBrowse( file_types=(('ckpt file', '.safetensors'),))],
+    [sg.Text("weight"),sg.Input(key="w6",right_click_menu=grp_rclick_menu["w6"]),sg.Text("vae"),sg.Radio("",key="v6",group_id='destination')],
+    [sg.Text("ckpt7"), sg.Input(key="ckpt7",right_click_menu=grp_rclick_menu["ckpt7"]),sg.FileBrowse( file_types=(('ckpt file', '.safetensors'),))],
+    [sg.Text("weight"),sg.Input(key="w7",right_click_menu=grp_rclick_menu["w7"]),sg.Text("vae"),sg.Radio("",key="v7",group_id='destination')],
     [sg.Text("output path"), sg.Input(key="out",right_click_menu=grp_rclick_menu["out"]),sg.FileSaveAs(file_types=(('ckpt file', '.safetensors'),))],
+    [sg.Text("infomation",key="info")],
     [sg.Button('RUN', key='RUN'),sg.Button('Cancel Vae', key='cancel'),sg.Button('EXIT', key='EXIT')]
 ]
 
@@ -143,7 +135,7 @@ while True:
                             window["w"+str(i+1)].update("1.0")
                     if values["v"+str(i+1)]:
                         v=i
-            thread1 = threading.Thread(target=mergeckpt,args=(ckpts,weights,v,out_path))
+            thread1 = threading.Thread(target=mergeckpt,args=(ckpts,weights,v,out_path,window))
             thread1.start()
     elif event=="cancel":
         for i in range(7):
