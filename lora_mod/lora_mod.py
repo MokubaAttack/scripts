05fd47d77579582c0bb30e25d5e46a1f1f3bc1ac
mokuba_attack <ninjapink24@gmail.com>
2026-01-19 21:55:20 +0900

    Update lora_mod.py

--- lora_mod/lora_mod.py
+++ lora_mod/lora_mod.py
@@ -1,30 +1,45 @@
-import os,sys
-import torch
 from safetensors.torch import load_file,save_file
+import sys
+import os
 import FreeSimpleGUI as sg
-import safetensors
 
 sg.theme('GrayGrayGray')
+ok=[
+    "lora_unet_out_2.alpha",
+    "lora_unet_out_2.lora_down.weight",
+    "lora_unet_out_2.lora_up.weight",
+    "lora_unet_input_blocks_0_0.alpha",
+    "lora_unet_input_blocks_0_0.lora_down.weight",
+    "lora_unet_input_blocks_0_0.lora_up.weight",
+    "lora_unet_label_emb_0_0.alpha",
+    "lora_unet_label_emb_0_0.lora_down.weight",
+    "lora_unet_label_emb_0_0.lora_up.weight",
+    "lora_unet_label_emb_0_2.alpha",
+    "lora_unet_label_emb_0_2.lora_down.weight",
+    "lora_unet_label_emb_0_2.lora_up.weight",
+    "lora_unet_time_embed_0.alpha",
+    "lora_unet_time_embed_0.lora_down.weight",
+    "lora_unet_time_embed_0.lora_up.weight",
+    "lora_unet_time_embed_2.alpha",
+    "lora_unet_time_embed_2.lora_down.weight",
+    "lora_unet_time_embed_2.lora_up.weight"
+]
+
 try:
     path = sys.argv[1]
 except:
-    path = sg.popup_get_file('ckpt file',title="get vae")
-
+    path = sg.popup_get_file('lora file',title="modify lora",file_types=(('lora file', '.safetensors'),))
+    
 if path!=None:
     if path.endswith(".safetensors"):
         try:
-            key="first_stage_model."
-            base_safe=path
-            out=base_safe.replace(".safetensors","_vae.safetensors")
-            state_dict=load_file(base_safe)
-            out_dict={}
-            for k,w in state_dict.items():
-                if k.startswith(key):
-                    k_out=k.replace(key,"")
-                    out_dict[k_out]=w.to(torch.float16)
-            f=sefetensors.safe_open(base_safe, framework="pt", device="cpu")
-            meta=f.metadata()
-            del f
-            save_file(out_dict,out,metadata=meta)
+            state_dict=load_file(path)
+            for k in ok:
+                if k in state_dict:
+                    out=state_dict.pop(k)
+            out_path=path.replace(".safetensors","_mod.safetensors")
+            save_file(state_dict,out_path)
+            del state_dict
         except:
-            sg.popup(path+" is failed.",title="error")
+            print(path+" is failed.")
+            
