a1c81b544339eb834db3364c43c96675f8196610
mokuba_attack <ninjapink24@gmail.com>
2026-01-24 11:41:06 +0900

    Update mokuup.py

--- mokuba_colab/mokuup/mokuup.py
+++ mokuba_colab/mokuup/mokuup.py
@@ -52,17 +52,15 @@ def plus_meta(vs,img):
     try:
         metadata=vs["pr"]+"\n\n"
         metadata=metadata+"Negative prompt: "+vs["ne"]+"\n\n"
-        metadata=metadata+"Steps: "+vs["st"]+", " 
+        metadata=metadata+"Steps: "+vs["st"]+", "
         metadata=metadata+"Sampler: "+vs["sa"]+", "
         metadata=metadata+"CFG scale: "+vs["cf"]+", "
         metadata=metadata+"Seed: "+vs["se"]+", "
         metadata=metadata+"Clip skip: "+vs["cl"]+", "
         metadata=metadata+"PAG scale: "+vs["pag"]+", "
-        if vs["ds"]!="":
-            metadata=metadata+"Denoising strength: "+vs["ds"]+", "
-
-        if vs["hum"]!="":
-            metadata=metadata+"Hires upscaler: "+vs["hum"]+", "
+        metadata=metadata+"Denoising strength: "+vs["ds"]+", "
+        metadata=metadata+"Tile upscale: "+vs["hu"]+", "
+        metadata=metadata+"Tile upscaler: "+vs["hum"]+", "
         metadata=metadata+'Civitai resources: ['
         if vs["ckpt"]!="":
             metadata=metadata+'{"type":"checkpoint","modelVersionId":'+vs["ckpt"]+"}"
@@ -84,7 +82,7 @@ def plus_meta(vs,img):
 
         if "[," in metadata:
             metadata=metadata.replace("[,","[")
-    
+
         image_path=vs["input"]
         if image_path.endswith(".jpg"):
             img.save(image_path, 'JPEG' ,quality=85)
@@ -102,7 +100,7 @@ def plus_meta(vs,img):
             img.save(image_path, "PNG")
 
 def tile_up(
-    img_paths=[],
+    img_path="",
     base_safe="ckpt.safetensors",
     vae_safe="vae.safetensors",
     loras=[],
@@ -119,24 +117,45 @@ def tile_up(
     pos_emb=[],
     neg_emb=[],
     pag=3.0,
+    url="",
+    out_folder="output",
+    j_or_p="j"
     ):
     prompt="masterpiece,best quality,ultra detailed"
     n_prompt="worst quality,low quality,normal quality"
-    memo=""
+    memo="seed\n"
     out_sizes=[]
     images=[]
-    if img_paths==[]:
-        print("Please select image files.")
+    img_paths=[]
+    if not(isinstance(seed, list)) or seed==[]:
+        seed=[0]
+    if img_path=="":
+        print("Please select a image file.")
         return []
     else:
-        for path in img_paths:
-            if os.path.exists(path):
-                memo=memo+path+" : ok\n"
-                img = Image.open(path)
+        for i in range(len(seed)):
+            try:
+                if img_path.startswith("https") or img_path.startswith("http"):
+                    p=io.BytesIO(requests.get(img_path).content)
+                else:
+                    p=img_path
+                img=Image.open(p)
                 images.append(img)
                 out_sizes.append([round(up*img.width/8)*8,round(up*img.height/8)*8])
-            else:
-                memo=memo+path+" : ng\n"
+                try:
+                    seed[i]=int(seed[i])
+                    if seed[i]==0:
+                        seed[i]=random.randint(1, sys.maxsize)
+                except:
+                    seed[i]=random.randint(1, sys.maxsize)
+                if j_or_p=="j":
+                    p=out_folder+"/"+str(i)+"_"+str(seed[i])+".jpg"
+                else:
+                    p=out_folder+"/"+str(i)+"_"+str(seed[i])+".png"
+                img_paths.append(p)
+                memo=memo+str(seed[i])+"\n"
+            except:
+                memo=memo+img_path+" : ng\n"
                 clear_output(True)
                 print(memo)
                 return []
@@ -145,22 +164,7 @@ def tile_up(
     print(memo)
 
     meta_dict={}
-    memo="seed\n"
-    if len(seed)<len(img_paths):
-        for i in range(len(img_paths)-len(seed)):
-            seed.append(seed[-1])
-    elif len(seed)>len(img_paths):
-        seed=seed[:len(img_paths)]
-    for i in range(len(seed)):
-        try:
-            seed[i]=int(seed[i])
-            if seed[i]==0:
-                seed[i]=random.randint(1, sys.maxsize)
-        except:
-            seed[i]=random.randint(1, sys.maxsize)
-        memo=memo+img_paths[i]+" : "+str(seed[i])+"\n"
-    clear_output(True)
-    print(memo)
+    meta_dict["hu"]=str(up)
         
     if not(os.path.isfile(base_safe)):
         print("the checkpoint file does not exist.")
@@ -241,6 +245,8 @@ def tile_up(
         meta_dict["lora"]=str(meta_id_list)
         meta_dict["w"]=str(meta_weight_list)
         del meta_id_list,meta_weight_list
+    else:
+        meta_dict["lora"]="[]"
     
     meta_embed_list=[]
     memo=memo+"Positive Embedding\n"
@@ -312,12 +318,24 @@ def tile_up(
             p=Image.LANCZOS
             meta_dict["hum"]="LANCZOS"
 
+    sd=load_file(base_safe)
+    is_sdxl="conditioner.embedders.1.model.transformer.resblocks.9.mlp.c_proj.weight" in sd
+    del sd
+    gc.collect()
+
     dtype=torch.float16
-    if os.path.isfile(vae_safe):
-        pipe = StableDiffusionXLPAGImg2ImgPipeline.from_single_file(base_safe, torch_dtype=dtype)
-        pipe.vae=AutoencoderKL.from_single_file(vae_safe,torch_dtype=dtype)
+    if is_sdxl:
+        if os.path.isfile(vae_safe):
+            pipe = StableDiffusionXLPAGImg2ImgPipeline.from_single_file(base_safe, torch_dtype=dtype)
+            pipe.vae=AutoencoderKL.from_single_file(vae_safe,torch_dtype=dtype)
+        else:
+            pipe = StableDiffusionXLPAGImg2ImgPipeline.from_single_file(base_safe, torch_dtype=dtype)
     else:
-        pipe = StableDiffusionXLPAGImg2ImgPipeline.from_single_file(base_safe, torch_dtype=dtype)
+        if os.path.isfile(vae_safe):
+            pipe = StableDiffusionPAGImg2ImgPipeline.from_single_file(base_safe, torch_dtype=dtype)
+            pipe.vae=AutoencoderKL.from_single_file(vae_safe,torch_dtype=dtype)
+        else:
+            pipe = StableDiffusionPAGImg2ImgPipeline.from_single_file(base_safe, torch_dtype=dtype)
     pipe.to("cuda")
 
     sgm_dict={}
@@ -500,7 +518,8 @@ def tile_up(
         else:
             p=Image.BILINEAR
             meta_dict["hum"]="BILINEAR"
-    memo=memo+"Hires upscaler : "+meta_dict["hum"]+"\n"
+    memo=memo+"Tile upscaler : "+meta_dict["hum"]+"\n"
+    memo=memo+"Tile upscale : "+meta_dict["hu"]+"\n"
 
     clear_output(True)
     print(memo)
@@ -521,10 +540,13 @@ def tile_up(
     if pos_emb!=[]:
         for line in pos_emb:
             key=os.path.basename(line).replace(".safetensors","")
-            state_dict = load_file(line)
-            pipe.load_textual_inversion(state_dict["clip_g"],token=key,text_encoder=pipe.text_encoder_2,tokenizer=pipe.tokenizer_2,torch_dtype=dtype)
-            pipe.load_textual_inversion(state_dict["clip_l"],token=key,text_encoder=pipe.text_encoder,tokenizer=pipe.tokenizer,torch_dtype=dtype)
-            del state_dict
+            if is_sdxl:
+                state_dict = load_file(line)
+                pipe.load_textual_inversion(state_dict["clip_g"],token=key,text_encoder=pipe.text_encoder_2,tokenizer=pipe.tokenizer_2,torch_dtype=dtype)
+                pipe.load_textual_inversion(state_dict["clip_l"],token=key,text_encoder=pipe.text_encoder,tokenizer=pipe.tokenizer,torch_dtype=dtype)
+                del state_dict
+            else:
+                pipe.load_textual_inversion(".", weight_name=line, token=key)
             prompt = prompt+","+key
             memo=memo+line+" is loaded.\n"
             clear_output(True)
@@ -534,10 +556,13 @@ def tile_up(
     if neg_emb!=[]:
         for line in neg_emb:
             key=os.path.basename(line).replace(".safetensors","")
-            state_dict = load_file(line)
-            pipe.load_textual_inversion(state_dict["clip_g"],token=key,text_encoder=pipe.text_encoder_2,tokenizer=pipe.tokenizer_2,torch_dtype=dtype)
-            pipe.load_textual_inversion(state_dict["clip_l"],token=key,text_encoder=pipe.text_encoder,tokenizer=pipe.tokenizer,torch_dtype=dtype)
-            del state_dict
+            if is_sdxl:
+                state_dict = load_file(line)
+                pipe.load_textual_inversion(state_dict["clip_g"],token=key,text_encoder=pipe.text_encoder_2,tokenizer=pipe.tokenizer_2,torch_dtype=dtype)
+                pipe.load_textual_inversion(state_dict["clip_l"],token=key,text_encoder=pipe.text_encoder,tokenizer=pipe.tokenizer,torch_dtype=dtype)
+                del state_dict
+            else:
+                pipe.load_textual_inversion(".", weight_name=line, token=key)
             n_prompt=n_prompt+","+key
             memo=memo+line+" is loaded.\n"
             clear_output(True)
@@ -554,20 +579,30 @@ def tile_up(
     pipe.vae.enable_tiling()
     pipe.enable_xformers_memory_efficient_attention()
     
-    comple = CompelForSDXL(pipe)
+    if is_sdxl:
+        comple = CompelForSDXL(pipe)
+    else:
+        comple = CompelForSD(pipe)
     conditioning = comple(prompt, negative_prompt=n_prompt)
-    prompts=[conditioning.embeds,conditioning.pooled_embeds,conditioning.negative_embeds,conditioning.negative_pooled_embeds]
+    if is_sdxl:
+        prompts=[conditioning.embeds,conditioning.pooled_embeds,conditioning.negative_embeds,conditioning.negative_pooled_embeds]
+    else:
+        prompts=[conditioning.embeds,conditioning.negative_embeds]
     del comple,conditioning
     gc.collect()
 
     pipe.text_encoder=None
-    pipe.text_encoder_2=None
     pipe.tokenizer=None
-    pipe.tokenizer_2=None
+    if is_sdxl:
+        pipe.text_encoder_2=None
+        pipe.tokenizer_2=None
 
     if p=="esrgan":
         uppipe=imgup(Interpolation)
     del Interpolation
+
+    if not(os.path.exists(out_folder)):
+        os.mkdir(out_folder)
 
     for i in range(len(seed)):
         if p=="esrgan":
@@ -584,7 +619,7 @@ def tile_up(
             tile_w = min(round(tile_h*aspect_ratio/8)*8, 1024)
         tile_w = max(512,tile_w)
         tile_h = max(512,tile_h)
-        overlap = min(64, tile_w // 8, tile_h // 8)
+        overlap = min( tile_w // 4, tile_h // 4)
 
         result = numpy.zeros((out_sizes[i][1], out_sizes[i][0], 3), dtype=numpy.float32)
         weight_sum = numpy.zeros((out_sizes[i][1], out_sizes[i][0], 1), dtype=numpy.float32)
@@ -601,25 +636,40 @@ def tile_up(
                 current_tile_size = (right - left,bottom - top)
 
                 tile = images[i].crop((left, top, right, bottom))
-                tile = tile.resize((tile_w, tile_h))
 
-                result_tile = pipe(
-                    eta=1.0,
-                    prompt_embeds=prompts[0],
-                    pooled_prompt_embeds=prompts[1],
-                    negative_prompt_embeds=prompts[2],
-                    negative_pooled_prompt_embeds=prompts[3],
-                    image=tile,
-                    guidance_scale=gs,
-                    generator=torch.manual_seed(seed[i]),
-                    num_inference_steps=step,
-                    clip_skip=cs,
-                    strength=ss,
-                    pag_scale=pag
-                ).images[0]
+                if is_sdxl:
+                    result_tile = pipe(
+                        eta=1.0,
+                        prompt_embeds=prompts[0],
+                        pooled_prompt_embeds=prompts[1],
+                        negative_prompt_embeds=prompts[2],
+                        negative_pooled_prompt_embeds=prompts[3],
+                        image=tile,
+                        guidance_scale=gs,
+                        generator=torch.manual_seed(seed[i]),
+                        num_inference_steps=int(step/ss)+1,
+                        clip_skip=cs,
+                        strength=ss,
+                        pag_scale=pag
+                    ).images[0]
+                else:
+                    result_tile = pipe(
+                        eta=1.0,
+                        prompt_embeds=prompts[0],
+                        negative_prompt_embeds=prompts[1],
+                        image=tile,
+                        guidance_scale=gs,
+                        generator=torch.manual_seed(seed[i]),
+                        num_inference_steps=int(step/ss)+1,
+                        clip_skip=cs,
+                        strength=ss,
+                        pag_scale=pag
+                    ).images[0]
+
+                if current_tile_size!=(result_tile.width,result_tile.height):
+                    result_tile = result_tile.resize( current_tile_size)
 
                 if current_tile_size != (tile_w, tile_h):
-                    result_tile = result_tile.resize( current_tile_size)
                     tile_weight = cv2.resize(gaussian_weight,current_tile_size)
                 else:
                     tile_weight = gaussian_weight[:current_tile_size[1], :current_tile_size[0]]
@@ -629,14 +679,17 @@ def tile_up(
                 weight_sum[top:bottom,left:right]=weight_sum[top:bottom,left:right]+tile_weight[:,:,numpy.newaxis]
         final_result = (result / weight_sum).astype(numpy.uint8)
         pil_image = Image.fromarray(final_result)
-        meta_dict["se"]=str(seed[i])
-        if img_paths[i].endswith(".jpg"):
-            meta_dict["input"]=img_paths[i].replace(".jpg","_up.jpg")
-        else:
-            meta_dict["input"]=img_paths[i].replace(".png","_up.png")
-        plus_meta(meta_dict,pil_image)
 
+        meta_dict["se"]=str(seed[i])
+        meta_dict["input"]=img_paths[i]
+        plus_meta(meta_dict,pil_image)
+        del pil_image,final_result,result,weight_sum
+        gc.collect()
         clear_output(True)
         print(memo)
-        display(pil_image)
+        display(image)
+    del images,pipe
+    if p=="esrgan":
+        del uppipe
+    gc.collect()
     return seed
