32193facab323656064730188b815b2bf4a7f7c0
mokuba_attack <ninjapink24@gmail.com>
2026-01-29 13:27:35 +0900

    Update setup.py

--- mokuba_colab/realersgan/setup.py
+++ mokuba_colab/realersgan/setup.py
@@ -1,7 +1,6 @@
-import py_real_esrgan,os,requests
-path=os.path.dirname(py_real_esrgan.__file__)+"/model.py"
-url="https://raw.githubusercontent.com/MokubaAttack/scripts/refs/heads/main/mokuba_colab/realersgan/model_mod.py"
-
+import basicsr,os
+path=os.path.dirname(basicsr.__file__)+"/data/degradations.py"
+url="https://raw.githubusercontent.com/MokubaAttack/scripts/refs/heads/main/mokuba_colab/realersgan/degradations_mod.py"
 urlData = requests.get(url).content
 with open(path ,mode='wb') as f:
     f.write(urlData)
