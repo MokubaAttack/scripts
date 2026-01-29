88618be51419a57309ebf4d10ea28c8ccea15d1f
mokuba_attack <ninjapink24@gmail.com>
2026-01-29 21:16:34 +0900

    Update setup.py

--- mokuba_colab/realersgan/setup.py
+++ mokuba_colab/realersgan/setup.py
@@ -1,7 +1,9 @@
 import os
 import torch
+import requests
 path=os.path.dirname(torch.__file__).replace("/torch","/basicsr/data/degradations.py")
 url="https://raw.githubusercontent.com/MokubaAttack/scripts/refs/heads/main/mokuba_colab/realersgan/degradations_mod.py"
 urlData = requests.get(url).content
 with open(path ,mode='wb') as f:
     f.write(urlData)
+
