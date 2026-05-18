64e72f41d27278be09408cdb05804cb478ecf113
mokuba_attack <ninjapink24@gmail.com>
2026-05-18 15:48:50 +0900

    Update merge_ckpt_anima.py

--- mokuba_colab/anima/scripts/merge_ckpt_anima.py
+++ mokuba_colab/anima/scripts/merge_ckpt_anima.py
@@ -176,155 +176,155 @@ def mergeckpt(ckpts,ws,out_path,mode="normal",win=None):
     if not(mode in ["normal","tensor1","tensor2"]):
         mode="normal"
 
-    #try:
-    if os.path.exists(os.getcwd()+"/safe_temp"):
-        shutil.rmtree(os.getcwd()+"/safe_temp")
-    os.mkdir(os.getcwd()+"/safe_temp")
-
-    c1=spckpt(ckpts[0],1)
-    c2=spckpt(ckpts[1],2)
-
-    data_dict=list(set(c1+c2))
-    dict_sum=len(data_dict)
-    key_count=0
-
-    with torch.no_grad():
+    try:
+        if os.path.exists(os.getcwd()+"/safe_temp"):
+            shutil.rmtree(os.getcwd()+"/safe_temp")
+        os.mkdir(os.getcwd()+"/safe_temp")
+    
+        c1=spckpt(ckpts[0],1)
+        c2=spckpt(ckpts[1],2)
+    
+        data_dict=list(set(c1+c2))
+        dict_sum=len(data_dict)
+        key_count=0
+    
+        with torch.no_grad():
+            for k in data_dict:
+                key_count=key_count+1
+                if win!=None:
+                    win["info"].update("merging "+str(key_count)+"/"+str(dict_sum))
+                else:
+                    print("\rmerging "+str(key_count)+"/"+str(dict_sum),end="")
+    
+                out_dict={}
+                if os.path.exists(os.getcwd()+"/safe_temp/1_"+k+".safetensors"):
+                    t1=load_file(os.getcwd()+"/safe_temp/1_"+k+".safetensors")[k].to(torch.float32)
+                else:
+                    t1=(load_file(os.getcwd()+"/safe_temp/2_"+k+".safetensors")[k]*0).to(torch.float32)
+    
+                if os.path.exists(os.getcwd()+"/safe_temp/2_"+k+".safetensors"):
+                    t2=load_file(os.getcwd()+"/safe_temp/2_"+k+".safetensors")[k].to(torch.float32)
+                else:
+                    t2=(load_file(os.getcwd()+"/safe_temp/1_"+k+".safetensors")[k]*0).to(torch.float32)
+    
+                if k.startswith("model.diffusion_model.blocks."):
+                    ind=int(k.split(".")[3])
+                    w=ws[ind+1]
+                elif k.startswith("model.diffusion_model.llm_adapter."):
+                    w=ws[29]
+                else:
+                    w=ws[0]
+    
+                if mode=="normal":
+                    out_dict[k]=((1-w)*t1+w*t2).to(torch.float16)
+    
+                elif "tensor" in mode:
+                    w1=(1-w)/2
+                    w2=w
+                    w1=round(t1.size()[0]*w1)
+                    w2=round(t1.size()[0]*(w1+w2))
+                    if w1==0:
+                        out_dict[k]=t2.to(torch.float16)
+                        save_file(out_dict,os.getcwd()+"/safe_temp/"+k+".safetensors")
+                        del w,out_dict,t1,t2,w1,w1
+                        del_safe(k)
+                        continue
+                    elif w2==0:
+                        out_dict[k]=t1.to(torch.float16)
+                        save_file(out_dict,os.getcwd()+"/safe_temp/"+k+".safetensors")
+                        del w,out_dict,t1,t2,w1,w1
+                        del_safe(k)
+                        continue
+                    if mode=="tensor1":
+                        if t1.dim()==1:
+                            t1[w1:w2]=t2[w1:w2]
+                        elif t1.dim()==2:
+                            t1[w1:w2,:]=t2[w1:w2,:]
+                        elif t1.dim()==3:
+                            t1[w1:w2,:,:]=t2[w1:w2,:,:]
+                        elif t1.dim()==4:
+                            t1[w1:w2,:,:,:]=t2[w1:w2,:,:,:]
+                        elif t1.dim()==5:
+                            t1[w1:w2,:,:,:,:]=t2[w1:w2,:,:,:,:]
+                    else:
+                        if t1.dim()==1:
+                            t1[w1:w2]=t2[w1:w2]
+                        elif t1.dim()==2:
+                            t1[:,w1:w2]=t2[:,w1:w2]
+                        elif t1.dim()==3:
+                            t1[:,w1:w2,:]=t2[:,w1:w2,:]
+                        elif t1.dim()==4:
+                            t1[:,w1:w2,:,:]=t2[:,w1:w2,:,:]
+                        elif t1.dim()==5:
+                            t1[:,w1:w2,:,:,:]=t2[:,w1:w2,:,:,:]
+                    out_dict[k]=t1.to(torch.float16)
+                    del w1,w2
+    
+                save_file(out_dict,os.getcwd()+"/safe_temp/"+k+".safetensors")
+                del w,out_dict,t1,t2
+                del_safe(k)
+    
+        if win==None:
+            print("")
+    
+        if win==None:
+            print("making output")
+        else:
+            win["info"].update("making output")
+        out_dict={}
+        out_dict["__metadata__"]={"format":"pt"}
+        n=0
+        for k in data_dict:
+            f=open(os.getcwd()+"/safe_temp/"+k+".safetensors","rb")
+            l=int.from_bytes(f.read(8),byteorder="little")
+            head=f.read(l).decode()
+            head=json.loads(head)
+            out_dict[k]=head[k]
+            offsets=out_dict[k]["data_offsets"][1]
+            out_dict[k]["data_offsets"][0]=n
+            n=n+offsets
+            out_dict[k]["data_offsets"][1]=n
+            f.close()
+    
+        output=open(out_path,"wb")
+        out_dict=str(out_dict).replace("'",'"')
+        out_dict=out_dict.encode()
+        l=len(out_dict).to_bytes(8,byteorder="little")
+        output.write(l)
+        output.write(out_dict)
+    
+        dict_sum=len(data_dict)
+        key_count=0
         for k in data_dict:
             key_count=key_count+1
-            if win!=None:
-                win["info"].update("merging "+str(key_count)+"/"+str(dict_sum))
+            if win==None:
+                print("\r"+str(key_count)+"/"+str(dict_sum),end="")
             else:
-                print("\rmerging "+str(key_count)+"/"+str(dict_sum),end="")
-
-            out_dict={}
-            if os.path.exists(os.getcwd()+"/safe_temp/1_"+k+".safetensors"):
-                t1=load_file(os.getcwd()+"/safe_temp/1_"+k+".safetensors")[k].to(torch.float32)
-            else:
-                t1=(load_file(os.getcwd()+"/safe_temp/2_"+k+".safetensors")[k]*0).to(torch.float32)
-
-            if os.path.exists(os.getcwd()+"/safe_temp/2_"+k+".safetensors"):
-                t2=load_file(os.getcwd()+"/safe_temp/2_"+k+".safetensors")[k].to(torch.float32)
-            else:
-                t2=(load_file(os.getcwd()+"/safe_temp/1_"+k+".safetensors")[k]*0).to(torch.float32)
-
-            if k.startswith("model.diffusion_model.blocks."):
-                ind=int(k.split(".")[3])
-                w=ws[ind+1]
-            elif k.startswith("model.diffusion_model.llm_adapter."):
-                w=ws[29]
-            else:
-                w=ws[0]
-
-            if mode=="normal":
-                out_dict[k]=((1-w)*t1+w*t2).to(torch.float16)
-
-            elif "tensor" in mode:
-                w1=(1-w)/2
-                w2=w
-                w1=round(t1.size()[0]*w1)
-                w2=round(t1.size()[0]*(w1+w2))
-                if w1==0:
-                    out_dict[k]=t2.to(torch.float16)
-                    save_file(out_dict,os.getcwd()+"/safe_temp/"+k+".safetensors")
-                    del w,out_dict,t1,t2,w1,w1
-                    del_safe(k)
-                    continue
-                elif w2==0:
-                    out_dict[k]=t1.to(torch.float16)
-                    save_file(out_dict,os.getcwd()+"/safe_temp/"+k+".safetensors")
-                    del w,out_dict,t1,t2,w1,w1
-                    del_safe(k)
-                    continue
-                if mode=="tensor1":
-                    if t1.dim()==1:
-                        t1[w1:w2]=t2[w1:w2]
-                    elif t1.dim()==2:
-                        t1[w1:w2,:]=t2[w1:w2,:]
-                    elif t1.dim()==3:
-                        t1[w1:w2,:,:]=t2[w1:w2,:,:]
-                    elif t1.dim()==4:
-                        t1[w1:w2,:,:,:]=t2[w1:w2,:,:,:]
-                    elif t1.dim()==5:
-                        t1[w1:w2,:,:,:,:]=t2[w1:w2,:,:,:,:]
-                else:
-                    if t1.dim()==1:
-                        t1[w1:w2]=t2[w1:w2]
-                    elif t1.dim()==2:
-                        t1[:,w1:w2]=t2[:,w1:w2]
-                    elif t1.dim()==3:
-                        t1[:,w1:w2,:]=t2[:,w1:w2,:]
-                    elif t1.dim()==4:
-                        t1[:,w1:w2,:,:]=t2[:,w1:w2,:,:]
-                    elif t1.dim()==5:
-                        t1[:,w1:w2,:,:,:]=t2[:,w1:w2,:,:,:]
-                out_dict[k]=t1.to(torch.float16)
-                del w1,w2
-
-            save_file(out_dict,os.getcwd()+"/safe_temp/"+k+".safetensors")
-            del w,out_dict,t1,t2
-            del_safe(k)
-
-    if win==None:
-        print("")
-
-    if win==None:
-        print("making output")
-    else:
-        win["info"].update("making output")
-    out_dict={}
-    out_dict["__metadata__"]={"format":"pt"}
-    n=0
-    for k in data_dict:
-        f=open(os.getcwd()+"/safe_temp/"+k+".safetensors","rb")
-        l=int.from_bytes(f.read(8),byteorder="little")
-        head=f.read(l).decode()
-        head=json.loads(head)
-        out_dict[k]=head[k]
-        offsets=out_dict[k]["data_offsets"][1]
-        out_dict[k]["data_offsets"][0]=n
-        n=n+offsets
-        out_dict[k]["data_offsets"][1]=n
+                win["info"].update("making output "+str(key_count)+"/"+str(dict_sum))
+            f=open(os.getcwd()+"/safe_temp/"+k+".safetensors","rb")
+            l=int.from_bytes(f.read(8),byteorder="little")
+            head=f.read(l)
+            output.write(f.read())
+            f.close()
+            os.remove(os.getcwd()+"/safe_temp/"+k+".safetensors")
+        output.close()
+            
+        f=open(out_path.replace(".safetensors",".txt"),"w")
+        for i in range(len(ckpts)):
+            f.write("ckpt"+str(i+1)+" : "+ckpts[i]+"\n")
+        f.write("weight : "+str(ws)+"\n")
         f.close()
-
-    output=open(out_path,"wb")
-    out_dict=str(out_dict).replace("'",'"')
-    out_dict=out_dict.encode()
-    l=len(out_dict).to_bytes(8,byteorder="little")
-    output.write(l)
-    output.write(out_dict)
-
-    dict_sum=len(data_dict)
-    key_count=0
-    for k in data_dict:
-        key_count=key_count+1
+        shutil.rmtree(os.getcwd()+"/safe_temp")
+        del out_dict,l,head,n,offsets
+        gc.collect()
         if win==None:
-            print("\r"+str(key_count)+"/"+str(dict_sum),end="")
+            print("")
+            print(out_path)
         else:
-            win["info"].update("making output "+str(key_count)+"/"+str(dict_sum))
-        f=open(os.getcwd()+"/safe_temp/"+k+".safetensors","rb")
-        l=int.from_bytes(f.read(8),byteorder="little")
-        head=f.read(l)
-        output.write(f.read())
-        f.close()
-        os.remove(os.getcwd()+"/safe_temp/"+k+".safetensors")
-    output.close()
-        
-    f=open(out_path.replace(".safetensors",".txt"),"w")
-    for i in range(len(ckpts)):
-        f.write("ckpt"+str(i+1)+" : "+ckpts[i]+"\n")
-    f.write("weight : "+str(ws)+"\n")
-    f.close()
-    shutil.rmtree(os.getcwd()+"/safe_temp")
-    del out_dict,l,head,n,offsets
-    gc.collect()
-    if win==None:
-        print("")
-        print(out_path)
-    else:
-        win["RUN"].Update(disabled=False)
-        win["info"].update("fin")
-        notification.notify(title="fin",message=out_path)
-    """
+            win["RUN"].Update(disabled=False)
+            win["info"].update("fin")
+            notification.notify(title="fin",message=out_path)
+
     except:
         if os.path.exists(os.getcwd()+"/safe_temp"):
             shutil.rmtree(os.getcwd()+"/safe_temp")
@@ -334,7 +334,6 @@ def mergeckpt(ckpts,ws,out_path,mode="normal",win=None):
             win["RUN"].Update(disabled=False)
             win["info"].update("error")
             notification.notify(title="error",message="I failed in the output.")
-    """
 
 if __name__=="__main__":
     import FreeSimpleGUI as sg
@@ -466,10 +465,10 @@ if __name__=="__main__":
                         window["w"+str(i+1)].update(str(weight))
                 else:
                     try:
-                        weight=float(values["w"])
+                        weight=float(values["w0"])
                     except:
                         weight=0.5
-                    window["w"].update(str(weight))
+                    window["w0"].update(str(weight))
                     weights=[]
                     for i in range(30):
                         weights.append(weight)
