62f9f68a503d3973122533c65813e8daed5126d8
mokuba_attack <ninjapink24@gmail.com>
2026-05-18 12:22:25 +0900

    Update __init__.py

--- mokuba_colab/source/mokucola/__init__.py
+++ mokuba_colab/source/mokucola/__init__.py
@@ -1,4 +1,31 @@
 try:
+    import transformers
+    import re
+
+    v=str(transformers.__version__)
+    match=re.match(r"([0-9]+)\.([0-9]+)\.([0-9]+)",v)
+    t1=str(match.group(1)).zfill(5)
+    t2=str(match.group(2)).zfill(5)
+    t3=str(match.group(3)).zfill(5)
+    v=t1+"."+t2+"."+t3
+except:
+    v="00001.00000.00000"
+if v<"00005.00005.00004":
+    import subprocess
+    import sys
+    import importlib
+
+    cmd=["pip","install","transformers==5.5.4"]
+    subprocess.run(cmd)
+
+    for k in list(sys.modules.keys()):
+        if k.startswith("transformers"):
+            try:
+                importlib.reload(sys.modules[k])
+            except:
+                pass
+
+try:
     from .workflow import (
         mokucola,
         mokuup
