00df876411d1731ca023e3ff03ac957eff045063
mokuba_attack <ninjapink24@gmail.com>
2026-06-22 10:09:41 +0900

    Update __init__.py

--- mokuba_colab/source/mokucola/__init__.py
+++ mokuba_colab/source/mokucola/__init__.py
@@ -1,20 +1,7 @@
-try:
-	import transformers
-	import re
+import subprocess
 
-	v=str(transformers.__version__)
-	match=re.match(r"([0-9]+)\.([0-9]+)\.([0-9]+)",v)
-	t1=str(match.group(1)).zfill(5)
-	t2=str(match.group(2)).zfill(5)
-	t3=str(match.group(3)).zfill(5)
-	v=t1+"."+t2+"."+t3
-except:
-	v="00001.00000.00000"
-if v<"00005.00005.00004":
-	import subprocess
-	
-	cmd=["pip","install","transformers==5.5.4"]
-	subprocess.run(cmd)
+cmd=["pip","install","transformers==5.5.4"]
+subprocess.run(cmd)
 
 import sys
 import importlib
